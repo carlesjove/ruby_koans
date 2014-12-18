3aa4b34626dddf6eb8eead5d29bf9ae218ced97d
Carles Jove i Buxeda <hola@carlus.cat>
2014-12-18 23:47:19 +0100

    added example of keyword method with required keyword. closes #142

--- src/about_keyword_arguments.rb
+++ src/about_keyword_arguments.rb
@@ -13,19 +13,30 @@ class AboutKeywordArguments < Neo::Koan
     assert_equal __([1, 2]), method_with_keyword_arguments(two: 2)
   end
 
-  def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
-    [one, two, three]
-  end
-
-  def test_keyword_arguments_with_wrong_number_of_arguments
-    exception = assert_raise (___(ArgumentError)) do
-      method_with_keyword_arguments_with_mandatory_argument
+  in_ruby_version("2.0") do
+    def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
+      [one, two, three]
     end
-    assert_match(/#{__("wrong number of arguments")}/, exception.message)
+
+    def test_keyword_arguments_with_wrong_number_of_arguments
+      exception = assert_raise (___(ArgumentError)) do
+        method_with_keyword_arguments_with_mandatory_argument
+      end
+      assert_match(/#{__("wrong number of arguments")}/, exception.message)
+    end
   end
 
-  # THINK ABOUT IT:
-  #
-  # Keyword arguments always have a default value, making them optional to the caller
+  in_ruby_version("2.1") do
+    def method_with_keyword_arguments_with_mandatory_keyword_argument(one:, two: 2, three: 3)
+      [one, two, three]
+    end
+
+    def test_keyword_arguments_with_wrong_number_of_arguments
+      exception = assert_raise (___(ArgumentError)) do
+        method_with_keyword_arguments_with_mandatory_keyword_argument
+      end
+      assert_match(/#{__("missing keyword")}/, exception.message)
+    end
+  end
 
 end
